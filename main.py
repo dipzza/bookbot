57aca2164214cbba2e7eb6dfb3599b5f813dfc97
dipzza <dipzza@disroot.org>
2025-02-26 14:11:23 +0100

    Use system arguments

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
+import sys
+from stats import get_num_words
+
+
 def main():
-  book_path = "books/frankenstein.txt"
-  text = get_file_text(book_path)
-  word_count = count_words(text)
-  char_count = count_characters(text)
-  char_count_sorted = chars_dict_to_sorted_list(char_count)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-  print(f"--- Begin report of {book_path} ---")
-  print(f"{word_count} words found in the document")
-  print()
+    book_path = sys.argv[1]
+    text = get_file_text(book_path)
+    word_count = get_num_words(text)
+    char_count = count_characters(text)
+    char_count_sorted = chars_dict_to_sorted_list(char_count)
 
-  for item in char_count_sorted:
-    if item["char"].isalpha():
-      print(f"The '{item['char']}' character was found {item['num']} times")
+    print(f"--- Begin report of {book_path} ---")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
 
-  print("--- End report ---")
+    for item in char_count_sorted:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
 
-def count_words(text):
-  words = text.split()
-  return len(words)
+    print("============= END ===============")
+
 
 def count_characters(text):
-  lower_case_text = text.lower()
-  character_counts = {}
+    lower_case_text = text.lower()
+    character_counts = {}
 
-  for char in lower_case_text:
-    if char in character_counts:
-      character_counts[char] += 1
-    else: 
-      character_counts[char] = 1
+    for char in lower_case_text:
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
 
-  return character_counts
+    return character_counts
+
 
 def sort_on(d):
     return d["num"]
+
 
 def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list = []
@@ -41,10 +49,10 @@ def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
-def get_file_text(path):
-  with open(path) as f:
-    return f.read()
 
+def get_file_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
